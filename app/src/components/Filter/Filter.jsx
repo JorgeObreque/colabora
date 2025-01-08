b968a2b5117ae677be55ee988da381768ba972bd
Jorge Obreque <jorge.obreque@ingresa.cl>
2025-01-08 14:26:01 -0300

    filters and GOB Style

---
+++ app/src/components/Filter/Filter.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect,useState} from "react";
+import './Filter.css';
+
+const Filter = () => {
+
+    return(
+        <div className="filter-main">
+            En 2021-2023
+        </div>
+    );
+}
+
+
+export default Filter;
