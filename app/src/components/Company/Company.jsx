1e9828f3e00dd2519ff50959d8f9f6a7f4428baa
Jorge Obreque <obrequel337@gmail.com>
2025-01-25 23:28:14 -0300

    feat: boards are added

--- app/src/components/Company/Company.jsx
+++ app/src/components/Company/Company.jsx
@@ -5,6 +5,8 @@ import Filter from "../Filter/Filter";
 import FilterGroup from "../FilterGroup/FilterGroup";
 import Board from "../Board/Board";
 import StarRating from "../StarRating/StarRating";
+import { AiOutlineDislike } from "react-icons/ai";
+import { AiOutlineLike } from "react-icons/ai";
 
 const Company = () =>{
     const styles = {color:"#E51E62",boxShadow: "0 0 0 0.2px #E51E62, 3px 3px 0px rgba(32, 115, 243, 0.4)"};
@@ -46,27 +48,64 @@ const Company = () =>{
                             de 4454 calificaciones recibidas
                         </div>
                     </Board>
-                    <div className="d-flex  flex-row gap-4">
-                        <Board size={''}>
+                    <div className="d-flex gap-3">
+                        <Board size={'100%'} className="g-col-6">
                             <div className="title">
                                 Reclamos de <b>mayo</b>
                             </div>
-                            <div className="claims-count">
-                                7.045
+                            <div className="claims-count py-3 d-flex justify-content-center align-items-center">
+                                <div>
+                                    7.045
+                                </div>
+                                <div>
+                                    <AiOutlineDislike size={'1em'} color={'red'} />
+                                </div>                                
                             </div>
                             <div className="info">
                                 <b>Empeoró</b> respecto a abril
                             </div>
                         </Board> 
-                        <Board size={''} >
+                        <Board size={'100%'} className="g-col-6">
                             <div className="title">
-                                {/* De <b>1 a 7 la nota promedio</b> que pusieron las personas a la respuesta de la empresa fue: */}
+                                Porcentaje de reclamos respondidos
                             </div>
-                            <div className="score">
-                                4,4
+                            <div className="claims-count py-3 d-flex justify-content-center align-items-center">
+                                <div>99%</div>    
+                                <div>
+                                    <AiOutlineLike size={'1em'} color={'green'} />
+                                </div>                             
                             </div>
                             <div className="info">
-                                de 4454 calificaciones recibidas
+                                <b>Mejoró</b> respecto a abril
+                            </div>
+                        </Board> 
+                    </div>
+                    <div className="d-flex gap-3">
+                        <Board size={'100%'} className="g-col-6">
+                            <div className="title">
+                                Reclamos con soluciones propuestas
+                            </div>
+                            <div className="claims-count py-3 d-flex justify-content-center align-items-center">
+                                <div>
+                                    59%
+                                </div>
+                                <div>
+                                    <AiOutlineDislike size={'1em'} color={'red'} />
+                                </div>                                
+                            </div>
+                            <div className="info">
+                                <b>Empeoró</b> respecto a abril
+                            </div>
+                        </Board> 
+                        <Board size={'100%'} className="g-col-6">
+                            <div className="title">
+                                Promedio de días que demoró en responder
+                            </div>
+                            <div className="claims-count py-3 d-flex justify-content-center align-items-center">
+                                <div className="py-3">4 días</div>                                
+                            </div>
+                            <div className="info">
+                                <b>Se mantiene</b> igual a abril
                             </div>
                         </Board> 
                     </div>
